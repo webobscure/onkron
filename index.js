33ba26e21dad418dee5fc628ea9eff0c960173b9
Alexandr <sparkygino@gmail.com>
2023-07-02 23:38:17 +0300

    Project done

--- index.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("https://shop.onkron.ru/images/new_listing/TS1991B-E/two_column_4.png").src= "https://shop.onkron.ru/images/new_listing/TS1991B-E/two_column_4.png";
-document.getElementById("Дом").innerText = "Дом";
