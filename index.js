177e6659d7fa42cff1017cd2ac7eefb78add0389
Alexandr <50839940+webobscure@users.noreply.github.com>
2023-07-03 00:43:16 +0400

    update files

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("https://shop.onkron.ru/images/new_listing/TS1991B-E/two_column_4.png").src= "https://shop.onkron.ru/images/new_listing/TS1991B-E/two_column_4.png";
+document.getElementById("Дом").innerText = "Дом";
+document.getElementById("Регулируемые по высоте колонны").src = "https://shop.onkron.ru/images/new_listing/TS1991B-E/resurce_6.png"
+document.getElementById("Две полки для дополнительного оборудования"). src = "assets/camera.png"; 
